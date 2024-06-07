7e1a9e0a9c6cb3a30e9053780e7e3dac7d084a89
dongrep <dongre.p@northeastern.edu>
2024-06-06 21:49:45 -0400

    Fixed the jenkins setup script

--- scripts/jenkins-jcasc-setup.sh
+++ scripts/jenkins-jcasc-setup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-cho "Installing Jenkins"
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list >/dev/null
+echo "Installing Jenkins"
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list >/dev/null
 sudo apt-get update
 sudo apt-get install jenkins -y
 
@@ -14,20 +11,24 @@ sudo apt install fontconfig openjdk-17-jre -y
 
 java -version
 
-echo "Starting Jenkins"
-sudo systemctl daemon-reload
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-
 echo "Setup Jenkins CLI"
 wget http://localhost:8080/jnlpJars/jenkins-cli.jar -O jenkins-cli.jar
 
-jenkins-plugin-cli --plugin-file ~/jenkins-scripts/plugins.txt
+echo "Installing necessary plugins"
+jenkins-plugin-cli --plugins configuration-as-code git github github-api job-dsl github-branch-source
 
+echo "Disabling Jenkins setup wizard"
 export JAVA_OPTS="-Djenkins.install.runSetupWizard=false"
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+
+echo "Copying JCasC configuration"
+sudo mkdir -p /var/jenkins_home
+sudo cp ~/jenkins/casc.yaml /var/jenkins_home/casc.yaml
 export CASC_JENKINS_CONFIG="/var/jenkins_home/casc.yaml"
 
-sudo cp ~/jenkins/casc.yaml /var/jenkins_home/casc.yaml
+echo "Starting Jenkins"
+sudo systemctl start jenkins
 
-echo "Restarting Jenkins"
+echo "Restarting Jenkins to apply configuration"
 sudo systemctl restart jenkins
