a5e269aaaeac0ad95eb4db738ad240cc666e3d2f
dongrep <dongre.p@northeastern.edu>
2024-06-12 16:04:49 -0400

    fix: fixed nodejs installation

--- scripts/jenkins-jcasc-setup.sh
+++ scripts/jenkins-jcasc-setup.sh
@@ -62,6 +62,7 @@ plugins=(
   docker-commons
   configuration-as-code
   job-dsl
+  nodejs
 )
 
 # Install the recommended plugins
@@ -100,6 +101,33 @@ sudo systemctl stop jenkins
 sudo systemctl start jenkins
 
 echo "Jenkins setup completed"
+
+echo "**************************************************************************"
+echo "*                                                                        *"
+echo "*                                                                        *"
+echo "*                           Installing Nodejs                            *"
+echo "*                                                                        *"
+echo "*                                                                        *"
+echo "**************************************************************************"
+
+# Download and import the Nodesource GPG key
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo \
+  gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+# Create a deb repository
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo \
+  tee /etc/apt/sources.list.d/nodesource.list
+
+# Run update and install
+sudo apt-get update && sudo apt-get install nodejs -y
+
+sleep 3
+
+# Check Node version:
+echo "Node $(node --version)"
 
 # Add Docker's official GPG key:
 echo "**************************************************************************"
